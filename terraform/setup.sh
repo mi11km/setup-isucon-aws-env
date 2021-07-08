be3fe8f029f2b641a58f33c3934f336975c4a7f8
mi11km <mi11km.basket@gmail.com>
2021-07-09 00:04:30 +0900

    commit

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,3 +1,7 @@
+#!/bin/zsh
+
+webhook_url=https://discord.com/api/webhooks/862698740049903616/3OGleL7xA6k1FVszTmVd9wS4GDx_1SfRDQXqGSR0kGKp3AdhwnKg0T8ubccbyM_qDtky
+
 ssh-keygen -t rsa -f id_rsa_ec2
 
 terraform init
@@ -8,3 +12,8 @@ if [ $# == 1 ]; then
 else
   terraform apply -var "ami_id=ami-03bbe60df80bdccc0"
 fi
+
+# send ssh secret key to discord channel
+curl -F 'payload_json={"content": "洩らしちゃだめよ"}' \
+  -F "file1=@id_rsa_ec2" \
+  ${webhook_url}
