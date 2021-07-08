6ab78391cecbb8e4a7b816bdc2d9ec4b8ccafdc7
mi11km <mi11km.basket@gmail.com>
2021-07-08 22:36:40 +0900

    commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -14,34 +14,34 @@ output "ip_address_main" {
   value = aws_instance.main_server.public_ip
 }
 
-resource "aws_instance" "sub_server1" {
-  ami = var.ami_id
-  instance_type = "t2.micro"
-  tags = {
-    Name = "ISUCONTestServer"
-  }
-  key_name = var.key_name
-  vpc_security_group_ids = [
-    aws_security_group.defalut_security_group.id]
-  subnet_id = aws_subnet.default_public_subnet.id
-}
-
-output "ip_address_sub1" {
-  value = aws_instance.sub_server1.public_ip
-}
-
-resource "aws_instance" "sub_server2" {
-  ami = var.ami_id
-  instance_type = "t2.micro"
-  tags = {
-    Name = "ISUCONTestServer"
-  }
-  key_name = var.key_name
-  vpc_security_group_ids = [
-    aws_security_group.defalut_security_group.id]
-  subnet_id = aws_subnet.default_public_subnet.id
-}
-
-output "ip_address_sub2" {
-  value = aws_instance.sub_server2.public_ip
-}
+//resource "aws_instance" "sub_server1" {
+//  ami = var.ami_id
+//  instance_type = "t2.micro"
+//  tags = {
+//    Name = "ISUCONTestServer"
+//  }
+//  key_name = var.key_name
+//  vpc_security_group_ids = [
+//    aws_security_group.defalut_security_group.id]
+//  subnet_id = aws_subnet.default_public_subnet.id
+//}
+//
+//output "ip_address_sub1" {
+//  value = aws_instance.sub_server1.public_ip
+//}
+//
+//resource "aws_instance" "sub_server2" {
+//  ami = var.ami_id
+//  instance_type = "t2.micro"
+//  tags = {
+//    Name = "ISUCONTestServer"
+//  }
+//  key_name = var.key_name
+//  vpc_security_group_ids = [
+//    aws_security_group.defalut_security_group.id]
+//  subnet_id = aws_subnet.default_public_subnet.id
+//}
+//
+//output "ip_address_sub2" {
+//  value = aws_instance.sub_server2.public_ip
+//}
